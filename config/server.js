f4f27a94420944ddc9fff6edaa79ec641b0f3a23
Harithya Wisesa <harithya77@gmail.com>
2021-12-25 16:28:29 +0700

    fix:update base url

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://blooming-taiga-09629.herokuapp.com/'),
+  host: env('https://blooming-taiga-09629.herokuapp.com/'),
   port: env.int('PORT', 1337),
 });
