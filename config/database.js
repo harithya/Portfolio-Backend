49e17a0551b28002f298e3da110a41bdb36e4585
Harithya Wisesa <harithya77@gmail.com>
2021-12-25 21:13:27 +0700

    fix-mengubah db menjadi potsgre

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'portfolio'),
-      user: env('DATABASE_USERNAME', 'root'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
