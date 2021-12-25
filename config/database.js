cc9922054d6d49b1154e1f0accd7368675f6dedd
Harithya Wisesa <harithya77@gmail.com>
2021-12-25 21:17:03 +0700

    fix-to mysql

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'portfolio'),
-      user: env('DATABASE_USERNAME', 'rgoot'),
+      user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
