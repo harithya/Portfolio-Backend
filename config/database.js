b0be0aa5b3426ae0f39a4ce9194e05496a88c3f7
Harithya Wisesa <harithya77@gmail.com>
2021-12-25 22:00:16 +0700

    fix-db

--- config/database.js
+++ config/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', ''),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false
       },
-      debug: false,
     },
+    debug: false,
   },
 });
