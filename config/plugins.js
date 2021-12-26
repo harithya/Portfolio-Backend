c218248c73bcc6d3218619a2c6ca4c16f350e36b
Harithya Wisesa <harithya77@gmail.com>
2021-12-26 07:15:03 +0700

    fix-change env storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,10 @@
 module.exports = ({ env }) => ({
-    // ...
     upload: {
         provider: 'cloudinary',
         providerOptions: {
-            cloud_name: env('cv-abdi-creative'),
-            api_key: env('388123733947478'),
-            api_secret: env('gn9RE9EqYDlsyFW6ANMmbwqxZoc'),
-        },
-        actionOptions: {
-            upload: {},
-            delete: {},
-        },
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+        }
     },
-    // ...
 });
